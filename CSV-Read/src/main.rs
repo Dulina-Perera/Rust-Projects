8f266fab1584bb751f496274a9389ddfffc0121a
Dulina-Perera <dulina.perera@outlook.com>
2024-03-18 11:49:16 +0530

    CSV-Read: Complete!

---
+++ CSV-Read/src/main.rs
@@ -0,0 +1,20 @@
+use std::error::Error;
+
+use csv;
+
+fn read_from_file(file_path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(file_path)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = read_from_file("./customers.csv") {
+        eprintln!("{}", err);
+    }
+}
