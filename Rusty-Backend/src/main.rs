24ca6999cb7c656b358fae8bd1892c26e6d232b1
Dulina-Perera <dulina.perera@outlook.com>
2024-03-20 04:40:29 +0530

    Rusty-Backend: Defined a POST route to buy pizzas and a PATCH route to update pizzas.

--- Rusty-Backend/src/main.rs
+++ Rusty-Backend/src/main.rs
@@ -5,11 +5,23 @@ async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas available!")
 }
 
+#[post("/pizzas")]
+async fn buy_pizza() -> impl Responder {
+    HttpResponse::Created().body("Buying a pizza!")
+}
+
+#[patch("/pizzas/{uuid}")]
+async fn update_pizza(uuid: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Updating pizza with uuid: {}", uuid))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(get_pizzas)
+            .service(buy_pizza)
+            .service(update_pizza)
     })
     .bind("127.0.0.1:8080")?
     .run()
